e4c0a467db9609d390f2f1b3afff1bdb053ba206
sashank_tirumala <stsashank6@gmail.com>
2023-03-11 02:42:08 -0500

    preliminary test setup

---
+++ tests/main.py
@@ -0,0 +1,3 @@
+from yaml_config_override import add_arguments
+if __name__ == "__main__":
+    conf = add_arguments()
---
+++ tests/test_override.py
@@ -0,0 +1,5 @@
+from yaml_config_override import get_arguments
+
+def test_no_config():
+    config = get_arguments()
+    pass
